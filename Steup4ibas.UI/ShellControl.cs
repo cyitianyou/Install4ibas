9e4aa90a8a03c5281a01543549ae89e8a50618bb
cyitianyou <cyitianyou@gmail.com>
2016-10-26 15:08:08 +0800

    父项添加默认事件

--- Steup4ibas.UI/ShellControl.cs
+++ Steup4ibas.UI/ShellControl.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Steup4ibas.Tools.i18n;
 
 namespace Steup4ibas.UI
 {
@@ -32,17 +33,67 @@ namespace Steup4ibas.UI
                 control.Dock = DockStyle.Fill;
                 this.CurrentControl = control;
                 this.setButtonsVisible(control.ButtonsVisibleStyle);
+                control.ShellControl = this;
                 return true;
             }
             catch (Exception error)
             {
                 return false;
             }
-                
+
         }
-        
+
+        private void btn_Next_Click(object sender, EventArgs e)
+        {
+            var flag = false;//子项控件事件是否执行
+            if (this.CurrentControl != null)
+                flag = this.CurrentControl.OnNextEvent(sender, e);
+            if (flag) return;//子项控件事件成功执行,返回
+            //子项控件事件未执行,可执行默认操作
+            //以下添加默认操作
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            var flag = false;//子项控件事件是否执行
+            if (this.CurrentControl != null)
+               flag= this.CurrentControl.OnCancelEvent(sender, e);
+            if (flag) return;//子项控件事件成功执行,返回
+            //子项控件事件未执行,可执行默认操作
+            //以下添加默认操作
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            var flag = false;//子项控件事件是否执行
+            if (this.CurrentControl != null)
+                flag = this.CurrentControl.OnBackEvent(sender, e);
+            if (flag) return;//子项控件事件成功执行,返回
+            //子项控件事件未执行,可执行默认操作
+            //以下添加默认操作
+            if (MessageBox.Show(
+            i18n.prop("$.Msg.ConfirmCancelInstallation", "是否确认取消安装"),
+            i18n.prop("$.Msg.Prompt","提示"),
+            MessageBoxButtons.OKCancel,
+            MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btn_Finish_Click(object sender, EventArgs e)
+        {
+            var flag = false;//子项控件事件是否执行
+            if (this.CurrentControl != null)
+                flag = this.CurrentControl.OnNextEvent(sender, e);
+            if (flag) return;//子项控件事件成功执行,返回
+            //子项控件事件未执行,可执行默认操作
+            //以下添加默认操作
+            Application.Exit();
+        }
 
 
-        
+
+
     }
 }
