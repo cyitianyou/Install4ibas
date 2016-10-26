4d3cfb04af7e0312107a9ac5fd70e5802a86505f
cyitianyou <cyitianyou@gmail.com>
2016-10-26 15:28:22 +0800

    添加IIS管理工具类

---
+++ Steup4ibas.Tools/IISManager/IISManager.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Win32;
+using System.Management;
+
+namespace Steup4ibas.Tools.IISManager
+{
+    internal abstract class IISManager
+    {
+        #region Properties
+        private string osVersion;
+        internal string OSVersion
+        {
+            get
+            {
+                if (osVersion == null)
+                {
+                    var ver = new System.Management.ManagementObjectSearcher("select Version from SoftwareLicensingService");
+                    var items = ver.Get();
+                    foreach (var item in items)
+                    {
+                        object version = item.GetPropertyValue("Version");
+                        osVersion = version.ToString();
+                    }
+                }
+                return osVersion;
+            }
+        }
+        protected string I386PackPath { get; set; }
+        public string InOptionalFilePath { get; set; }
+        public string UnOptionalFilePath { get; set; }
+        /// <summary>
+        /// 注册表安装查找路径
+        /// </summary>
+        protected string regServicePackSourcePath;
+        /// <summary>
+        /// 注册表安装查找路径
+        /// </summary>
+        protected string regSourcePath;
+        /// <summary>
+        /// 安装IIS执行命令（后面应补充上配置文件TXT的文件路径）
+        /// </summary>
+        protected string SysocmgrCmd { get; set; }
+        #endregion
+
+        #region Methods
+        internal virtual void InstallIIS() { }
+        internal virtual void UnInstallIIS() { }
+        /// <summary>
+        /// 修改安装查找路径
+        /// </summary>
+        protected void AmendRegeditPath()
+        {
+            RegistryKey pRegKey = Registry.LocalMachine;
+            pRegKey = pRegKey.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Setup", true);
+            regServicePackSourcePath = pRegKey.GetValue("ServicePackSourcePath").ToString();
+            regSourcePath = pRegKey.GetValue("SourcePath").ToString();
+            pRegKey.SetValue("ServicePackSourcePath", I386PackPath);
+            pRegKey.SetValue("SourcePath", I386PackPath);
+        }
+        /// <summary>
+        /// 恢复安装查找路径
+        /// </summary>
+        protected void ReStoreRegeditPath()
+        {
+            RegistryKey pRegKey = Registry.LocalMachine;
+            pRegKey = pRegKey.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Setup", true);
+            pRegKey.SetValue("ServicePackSourcePath", regServicePackSourcePath);
+            pRegKey.SetValue("SourcePath", regSourcePath);
+        }
+        /// <summary>
+        /// 在ASP.NET中注册IIS
+        /// </summary>
+        protected void RegIISForAspnet()
+        {
+            if (CheckOSBitness.Is64BitOperatingSystem())
+                ExecuteCmd(Path.Combine(System.Environment.GetEnvironmentVariable("windir"), @"Microsoft.Net\Framework64\v2.0.50727\aspnet_regiis -i"));
+            else
+                ExecuteCmd(Path.Combine(System.Environment.GetEnvironmentVariable("windir"), @"Microsoft.Net\Framework\v2.0.50727\aspnet_regiis -i"));
+        }
+        /// <summary>
+        /// 执行命令行
+        /// </summary>
+        /// <param name="optionalFilePaths">命令</param>
+        /// <returns>返回结果</returns>
+        protected virtual string ExecuteCmd(string[] optionalFilePaths)
+        {
+            //运行命令行
+            Process p = new Process();
+            // 设定程序名
+            p.StartInfo.FileName = "cmd.exe";
+            // 关闭Shell的使用
+            p.StartInfo.UseShellExecute = false;
+            // 重定向标准输入
+            p.StartInfo.RedirectStandardInput = true;
+            // 重定向标准输出
+            p.StartInfo.RedirectStandardOutput = true;
+            //重定向错误输出
+            p.StartInfo.RedirectStandardError = true;
+            // 设置不显示窗口
+            p.StartInfo.CreateNoWindow = true;
+            // 启动进程
+            p.Start();
+            for (int i = 0; i < optionalFilePaths.Length; i++)
+            {
+                p.StandardInput.WriteLine(optionalFilePaths[i]);
+            }
+            p.StandardInput.WriteLine("exit");
+            // 从输出流获取命令执行结果
+            string strRst = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            p.Close();
+            return strRst;
+        }
+        /// <summary>
+        /// 执行命令行
+        /// </summary>
+        /// <param name="optionalFilePath">命令</param>
+        /// <returns>optionalFilePaths</returns>
+        protected virtual string ExecuteCmd(string optionalFilePath)
+        {
+            string[] optionalFilePaths = new string[] { optionalFilePath };
+            return ExecuteCmd(optionalFilePaths);
+        }
+
+
+        #endregion
+    }
+}
---
+++ Steup4ibas.Tools/IISManager/IISRuntime.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using System.IO;
+
+namespace Steup4ibas.Tools.IISManager
+{
+    public class IISRuntime
+    {
+        internal delegate void InstallIIS();
+        internal delegate void UnInstallIIS();
+        private string operatingSystemName;
+        /// <summary>
+        /// 操作系统名称
+        /// </summary>
+        protected string OperatingSystemName
+        {
+            get
+            {
+                if (operatingSystemName == null)
+                {
+                    operatingSystemName = CheckOSInfo.GetOSName();
+                }
+                return operatingSystemName;
+            }
+        }
+
+        InstallIIS installIIS;
+        UnInstallIIS unInstallIIS;
+        IISManager iisManager;
+
+        public IISRuntime()
+        {
+            bool is64Bit = CheckOSBitness.Is64BitOperatingSystem();
+            string setupPackPath = is64Bit ?
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Source\" + OperatingSystemName + @" x64") :
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Source\" + OperatingSystemName);
+            string inOptionalFilePath = is64Bit ?
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Source\" + OperatingSystemName + @" x64\Config\Install.txt") :
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Source\" + OperatingSystemName + @"\Config\Install.txt");
+            string unOptionalFilePath = is64Bit ?
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Source\" + OperatingSystemName + @" x64\Config\UnInstall.txt") :
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Source\" + OperatingSystemName + @"\Config\UnInstall.txt");
+
+            switch (OperatingSystemName)
+            {
+                case "Microsoft Windows XP":
+                case "Microsoft Windows Server 2003":
+                case "Microsoft Windows Server 2003 R2":
+                    iisManager = new IISManagerXP(setupPackPath, inOptionalFilePath, unOptionalFilePath);
+                    break;
+                case "Microsoft Windows 7":
+                case "Microsoft Windows Server 2008":
+                case "Microsoft Windows Server 2008 R2":
+                case "Microsoft Windows Vista":
+                    iisManager = new IISManager7();
+                    break;
+            }
+            installIIS = new InstallIIS(iisManager.InstallIIS);
+            unInstallIIS = new UnInstallIIS(iisManager.UnInstallIIS);
+        }
+
+        public void InstallIISRun()
+        {
+            Console.WriteLine(DateTime.Now + "IIS Manager {0} Begin!", OperatingSystemName);
+            installIIS();
+            Console.WriteLine(DateTime.Now + "IIS Installer {0} Success!", OperatingSystemName);
+        }
+        public void UnInstallIISRun()
+        {
+            Console.WriteLine(DateTime.Now + "IIS Manager {0} Begin!", OperatingSystemName);
+            unInstallIIS();
+            Console.WriteLine(DateTime.Now + "IIS UnInstall {0} Success!", OperatingSystemName);
+        }
+    }
+}
