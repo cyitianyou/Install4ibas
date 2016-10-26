f5651424dbe390095952245a5934975397ac4640
cyitianyou <cyitianyou@gmail.com>
2016-10-26 10:47:07 +0800

    上传项目文件

---
+++ Steup4ibas.UI/IUIAction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Steup4ibas.UI
+{
+    public interface IUIAction
+    {
+        /// <summary>
+        /// 完成
+        /// </summary>
+        event EventHandler FinishEvent;
+        /// <summary>
+        /// 上一步
+        /// </summary>
+        event EventHandler BackEvent;
+        /// <summary>
+        /// 下一步
+        /// </summary>
+        event EventHandler NextEvent;
+        /// <summary>
+        /// 取消
+        /// </summary>
+        event EventHandler CancelEvent;
+
+    }
+}
--- Steup4ibas.UI/ShellControl.cs
+++ Steup4ibas.UI/ShellControl.cs
@@ -19,7 +19,26 @@ namespace Steup4ibas.UI
 
         protected override void InitializeEvent()
         {
-        
+            this.FinishEvent += ShellControl_FinishEvent;
+            this.BackEvent += ShellControl_BackEvent;
+            this.NextEvent += ShellControl_NextEvent;
+            this.CancelEvent += ShellControl_CancelEvent;
+        }
+
+        void ShellControl_CancelEvent(object sender, EventArgs e)
+        {
+        }
+
+        void ShellControl_NextEvent(object sender, EventArgs e)
+        {
+        }
+
+        void ShellControl_BackEvent(object sender, EventArgs e)
+        {
+        }
+
+        void ShellControl_FinishEvent(object sender, EventArgs e)
+        {
         }
     }
 }
