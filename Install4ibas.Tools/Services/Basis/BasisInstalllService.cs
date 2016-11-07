e9e0b3a9c32b2883f30a451afc002e131d4a080c
cyitianyou <cyitianyou@gmail.com>
2016-11-07 16:35:55 +0800

    定义安装步骤

--- Install4ibas.Tools/Services/Basis/BasisInstalllService.cs
+++ Install4ibas.Tools/Services/Basis/BasisInstalllService.cs
@@ -1,4 +1,5 @@
 ﻿using Install4ibas.Tools.Common.InstallInformation;
+using Install4ibas.Tools.Services.Basis.Step;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,17 +19,20 @@ namespace Install4ibas.Tools.Services.Basis
                 return _AppSetting;
             }
         }
-        /// <summary>
-        /// 检查我的初始化状态
-        /// </summary>
-        protected virtual void CheckMyInitializedStatus()
+        InstallStepManager _StepManager;
+        public InstallStepManager StepManager
         {
-            if (this._AppSetting == null)
-                throw new Exception("尚未初始化设置。");
+            get
+            {
+                if (_StepManager == null)
+                    _StepManager = new InstallStepManager();
+                return _StepManager;
+            }
         }
+
         public bool Excute()
         {
-            this.CheckMyInitializedStatus();
+            this.BeforeExcute();
             try
             {
                 foreach (var item in this.AppSetting.Steps)
@@ -54,7 +58,19 @@ namespace Install4ibas.Tools.Services.Basis
             }
         }
 
-        protected abstract bool ExecutingStep(InstallInformationStep step);
+        protected virtual bool ExecutingStep(InstallInformationStep step)
+        {
+            try
+            {
+                var installStep = this.StepManager.GetInstallStep(step.StepCode);
+                installStep.Excute();
+                return true;
+            }
+            catch (Exception error)
+            {
+                return false;
+            }
+        }
         protected virtual void ExecutingStepDone(InstallInformationStep step)
         {
             step.Completed = true;
@@ -67,5 +83,54 @@ namespace Install4ibas.Tools.Services.Basis
 
 
         public event Common.ServiceEventHandle UpdateInstallationScheduleEvent;
+
+        /// <summary>
+        /// 检查我的初始化状态
+        /// </summary>
+        protected virtual void CheckMyInitializedStatus()
+        {
+            if (this._AppSetting == null)
+                throw new Exception("尚未初始化设置。");
+        }
+        protected virtual void BeforeExcute()
+        {
+            this.CheckMyInitializedStatus();
+            var information = this.GetPresetInstallInformation();
+            if (information != null)
+                this.AppSetting.Steps = information.Steps;
+            this.StepManager.SetAppSetting(this.AppSetting);
+        }
+        /// <summary>
+        /// 获取预设的安装信息
+        /// </summary>
+        /// <returns></returns>
+        protected virtual InstallInformation GetPresetInstallInformation()
+        {
+            try
+            {
+                var myAssembly = this.GetType().Assembly;
+                var myNamespace = this.GetType().Namespace;
+                System.IO.Stream setStream = null;
+                foreach (var item in myAssembly.GetManifestResourceNames())
+                {
+                    if (item.IndexOf(myNamespace) < 0) continue;
+                    if (!item.EndsWith("InstallInformationSteps.xml", StringComparison.InvariantCultureIgnoreCase)) continue;
+                    setStream = myAssembly.GetManifestResourceStream(item);
+                }
+                if (setStream == null)
+                    throw new Exception(string.Format("没有找到预置文件资源。", ""));
+                var knownTypes = new List<Type>() { typeof(InstallInformation), typeof(InstallInformationStep) };
+                var serializer = new System.Runtime.Serialization.DataContractSerializer(typeof(InstallInformation), knownTypes);
+                var information = serializer.ReadObject(setStream) as InstallInformation;
+                if (information == null)
+                    throw new Exception("预置方案配置不是有效的数据。");
+                setStream.Close();
+                return information;
+            }
+            catch (Exception error)
+            {
+                throw new Exception(string.Format("获取预置方案失败，{0}", error.Message), error);
+            }
+        }
     }
 }
--- Install4ibas.Tools/Services/Basis/Step/IInstallStep.cs
+++ Install4ibas.Tools/Services/Basis/Step/IInstallStep.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Install4ibas.Tools.Common.InstallInformation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +8,10 @@ namespace Install4ibas.Tools.Services.Basis
 {
     public interface IInstallStep
     {
+        string StepCode { get; }
+        string StepName { get; }
+        InstallInformation AppSetting { get; set; }
+
+        bool Excute();
     }
 }
