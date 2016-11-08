5bbacd38370ee5c4bbbcd43dc62b763b690774df
GSGSS <342517845@qq.com>
2016-11-08 11:07:07 +0800

    步骤类新建

---
+++ Install4ibas.Tools/Services/Basis/Step/InstallStep_ServicesInforEdit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Install4ibas.Tools.Services.Basis.Step
+{
+    class InstallStep_ServicesInforEdit
+    {
+    }
+}
