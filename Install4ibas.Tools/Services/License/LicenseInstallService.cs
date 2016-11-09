692f393cba64d032597a8c318af3111032ac371a
cyitianyou <cyitianyou@gmail.com>
2016-11-09 15:18:16 +0800

    添加License服务

---
+++ Install4ibas.Tools/Services/License/LicenseInstallService.cs
@@ -0,0 +1,13 @@
+﻿using Install4ibas.Tools.Services.Basis;
+
+namespace Install4ibas.Tools.Services.License
+{
+    class LicenseInstallService : BasisInstalllService
+    {
+        public const string SERVICECODE = "License";
+        public override string ServiceCode
+        {
+            get { return SERVICECODE; }
+        }
+    }
+}
