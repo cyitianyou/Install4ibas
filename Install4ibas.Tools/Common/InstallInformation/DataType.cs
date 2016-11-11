171664fa5cbdc922474d45b308daad0e6be31a06
cyitianyou <cyitianyou@gmail.com>
2016-11-11 14:12:16 +0800

    修改ibasModule

--- Install4ibas.Tools/Common/InstallInformation/DataType.cs
+++ Install4ibas.Tools/Common/InstallInformation/DataType.cs
@@ -19,9 +19,48 @@ namespace Install4ibas.Tools.Common.InstallInformation
     {
         all, hana, mssql, mysql, oracle, pgsql
     }
-
+    /// <summary>
+    /// 模块类型
+    /// </summary>
     public enum emModuleType
     {
-        all, shell, basic, standard, other
+        /// <summary>
+        /// 不确定
+        /// </summary>
+        all, 
+        /// <summary>
+        /// 壳
+        /// </summary>
+        shell,
+        /// <summary>
+        /// 基础模块
+        /// </summary>
+        basic, 
+        /// <summary>
+        /// 标准模块
+        /// </summary>
+        standard, 
+        /// <summary>
+        /// 其他模块
+        /// </summary>
+        other
+    }
+    /// <summary>
+    /// 安装状态
+    /// </summary>
+    public enum emInstallStatus
+    {
+        /// <summary>
+        /// 未安装
+        /// </summary>
+        notInstalled,
+        /// <summary>
+        /// 已安装
+        /// </summary>
+        Installed,
+        /// <summary>
+        /// 可更新
+        /// </summary>
+        canUpdate
     }
 }
--- Install4ibas.Tools/Common/InstallInformation/Module.cs
+++ Install4ibas.Tools/Common/InstallInformation/Module.cs
@@ -1,42 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
 
 namespace Install4ibas.Tools.Common.InstallInformation
 {
-  public  class ibasModule
+
+    [CollectionDataContract(Name = "ibasModules", Namespace = "http://ibas.club/install")]
+    [KnownType(typeof(ibasModules))]
+    [KnownType(typeof(ibasModule))]
+    public class ibasModules : ObservableCollection<ibasModule>, IList<ibasModule>
     {
-        public String ModuleName
+
+    }
+    [DataContract(Namespace = "http://ibas.club/install")]
+    public class ibasModule
+    {
+        const string DOWNLOAD = "联网下载";
+        public ibasModule()
         {
-            set;
-            get;
+            this.ModuleId = "";
+            this.ModuleName = "";
+            this.ModuleDescription = "";
+            this.ModuleInstallPath = "";
+            this.PackageFilePath = "";
+            this.PackageFileList = new MyList<string>();
+            this.Type = emModuleType.all;
+            this.Status = emInstallStatus.notInstalled;
         }
-        public String ModuleDescription
-        {
-            set;
-            get;
-        }
-        public String ModuleId
-        {
-            set;
-            get;
-        }
-        public String ModuleInstallPath
-        {
-            set;
-            get;
-        }
+        [DataMember(Name = "ModuleId", Order = 1)]
+        public String ModuleId { get; set; }
+        [DataMember(Name = "ModuleName", Order = 2)]
+        public String ModuleName { get; set; }
+        [DataMember(Name = "ModuleDescription", Order = 3)]
+        public String ModuleDescription { get; set; }
+        [DataMember(Name = "ModuleInstallPath", Order = 4)]
+        public String ModuleInstallPath { get; set; }
+        [DataMember(Name = "Type", Order = 5)]
         public emModuleType Type
         {
-            set;
-            get;
+            get
+            {
+                return _Type;
+            }
+            set
+            {
+                _Type = value;
+                if (_Type == emModuleType.basic
+                    || _Type == emModuleType.shell
+                    || _Type == emModuleType.standard)
+                {
+                    if (PackageFileList == null) PackageFileList = new MyList<string>();
+                    if (!PackageFileList.Contains(DOWNLOAD))
+                        PackageFileList.Add(DOWNLOAD);
+                }
+            }
         }
+        private emModuleType _Type;
+        [DataMember(Name = "Status", Order = 6)]
+        public emInstallStatus Status { get; set; }
+        [DataMember(Name = "PackageFilePath", Order = 7)]
+        public string PackageFilePath;
+        [DataMember(Name = "PackageFileList", Order = 8)]
+        public MyList<string> PackageFileList;
 
-        public bool IsInstalled
-        {
-            set;
-            get;
-        }
+
+    }
+    [CollectionDataContract(ItemName = "FilePath", Namespace = "http://ibas.club/install")]
+    public class MyList<T> : List<T>
+    {
+
     }
 }
