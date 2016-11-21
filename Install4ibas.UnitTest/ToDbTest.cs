02c36b4bcfb039d95faa780b5f7053fd584a2d61
GSGSS <342517845@qq.com>
2016-11-21 11:00:37 +0800

    11

---
+++ Install4ibas.UnitTest/ToDbTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Install4ibas.Tools.Common.InstallInformation;
+
+namespace Install4ibas.UnitTest
+{
+    [TestClass]
+    public class ToDbTest
+    {
+        [TestMethod]
+        public void ToDbTestMethod()
+        {
+            
+         
+        }
+    }
+}
