dfe607f779125387c92dfdbed778ce8b95afa650
GSGSS <342517845@qq.com>
2016-12-16 17:16:57 +0800

    11

--- Install4ibas.UI/FormControl/LicenseCreateControl.Designer.cs
+++ Install4ibas.UI/FormControl/LicenseCreateControl.Designer.cs
@@ -37,9 +37,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_webInfo = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txt_LicenseInfo = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -57,6 +57,7 @@
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -72,15 +73,17 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
-            this.splitContainer1.Size = new System.Drawing.Size(650, 440);
+            this.splitContainer1.Size = new System.Drawing.Size(433, 311);
+            this.splitContainer1.SplitterWidth = 3;
             this.splitContainer1.TabIndex = 2;
             // 
             // btn_ChooseFolder
             // 
             this.btn_ChooseFolder.Font = new System.Drawing.Font("宋体", 4F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.btn_ChooseFolder.Location = new System.Drawing.Point(500, 13);
+            this.btn_ChooseFolder.Location = new System.Drawing.Point(333, 9);
+            this.btn_ChooseFolder.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_ChooseFolder.Name = "btn_ChooseFolder";
-            this.btn_ChooseFolder.Size = new System.Drawing.Size(25, 28);
+            this.btn_ChooseFolder.Size = new System.Drawing.Size(17, 20);
             this.btn_ChooseFolder.TabIndex = 8;
             this.btn_ChooseFolder.Text = "...";
             this.btn_ChooseFolder.UseVisualStyleBackColor = true;
@@ -88,9 +91,10 @@
             // 
             // btn_Import
             // 
-            this.btn_Import.Location = new System.Drawing.Point(542, 13);
+            this.btn_Import.Location = new System.Drawing.Point(361, 9);
+            this.btn_Import.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Import.Name = "btn_Import";
-            this.btn_Import.Size = new System.Drawing.Size(75, 28);
+            this.btn_Import.Size = new System.Drawing.Size(50, 20);
             this.btn_Import.TabIndex = 7;
             this.btn_Import.Text = "导入";
             this.btn_Import.UseVisualStyleBackColor = true;
@@ -98,33 +102,37 @@
             // 
             // txt_Path
             // 
-            this.txt_Path.Location = new System.Drawing.Point(356, 13);
+            this.txt_Path.Location = new System.Drawing.Point(237, 9);
+            this.txt_Path.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_Path.Name = "txt_Path";
-            this.txt_Path.Size = new System.Drawing.Size(138, 27);
+            this.txt_Path.Size = new System.Drawing.Size(93, 21);
             this.txt_Path.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(299, 17);
+            this.label2.Location = new System.Drawing.Point(199, 12);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 17);
+            this.label2.Size = new System.Drawing.Size(35, 12);
             this.label2.TabIndex = 4;
             this.label2.Text = "路径:";
             // 
             // txt_SiteName
             // 
-            this.txt_SiteName.Location = new System.Drawing.Point(69, 14);
+            this.txt_SiteName.Location = new System.Drawing.Point(46, 10);
+            this.txt_SiteName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_SiteName.Name = "txt_SiteName";
-            this.txt_SiteName.Size = new System.Drawing.Size(179, 27);
+            this.txt_SiteName.Size = new System.Drawing.Size(121, 21);
             this.txt_SiteName.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 17);
+            this.label1.Location = new System.Drawing.Point(14, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.Size = new System.Drawing.Size(29, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "网站";
             // 
@@ -133,71 +141,80 @@
             this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer2.IsSplitterFixed = true;
             this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.splitContainer2.Name = "splitContainer2";
             // 
             // splitContainer2.Panel1
             // 
             this.splitContainer2.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer2.Panel1Collapsed = true;
             // 
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.Controls.Add(this.groupBox2);
-            this.splitContainer2.Panel2Collapsed = true;
-            this.splitContainer2.Size = new System.Drawing.Size(650, 386);
-            this.splitContainer2.SplitterDistance = 620;
+            this.splitContainer2.Size = new System.Drawing.Size(433, 258);
+            this.splitContainer2.SplitterDistance = 405;
+            this.splitContainer2.SplitterWidth = 3;
             this.splitContainer2.TabIndex = 3;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txt_webInfo);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(650, 386);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(405, 258);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "账套信息";
             // 
-            // textBox1
+            // txt_webInfo
             // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(3, 23);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(644, 360);
-            this.textBox1.TabIndex = 0;
+            this.txt_webInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt_webInfo.Location = new System.Drawing.Point(2, 16);
+            this.txt_webInfo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_webInfo.Multiline = true;
+            this.txt_webInfo.Name = "txt_webInfo";
+            this.txt_webInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_webInfo.Size = new System.Drawing.Size(401, 240);
+            this.txt_webInfo.TabIndex = 0;
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.txt_LicenseInfo);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(96, 100);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(433, 258);
             this.groupBox2.TabIndex = 0;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "许可信息";
             // 
-            // textBox2
+            // txt_LicenseInfo
             // 
-            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox2.Font = new System.Drawing.Font("宋体", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox2.Location = new System.Drawing.Point(3, 23);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox2.Size = new System.Drawing.Size(90, 74);
-            this.textBox2.TabIndex = 1;
+            this.txt_LicenseInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt_LicenseInfo.Font = new System.Drawing.Font("宋体", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt_LicenseInfo.Location = new System.Drawing.Point(2, 16);
+            this.txt_LicenseInfo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_LicenseInfo.Multiline = true;
+            this.txt_LicenseInfo.Name = "txt_LicenseInfo";
+            this.txt_LicenseInfo.ReadOnly = true;
+            this.txt_LicenseInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_LicenseInfo.Size = new System.Drawing.Size(429, 240);
+            this.txt_LicenseInfo.TabIndex = 1;
             // 
             // LicenseCreateControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "LicenseCreateControl";
-            this.Size = new System.Drawing.Size(650, 440);
+            this.Size = new System.Drawing.Size(433, 311);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
@@ -225,9 +242,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.SplitContainer splitContainer2;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txt_webInfo;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txt_LicenseInfo;
         private System.Windows.Forms.Button btn_ChooseFolder;
 
 
--- Install4ibas.UI/FormControl/LicenseCreateControl.cs
+++ Install4ibas.UI/FormControl/LicenseCreateControl.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Install4ibas.UI
 {
@@ -24,12 +25,44 @@ namespace Install4ibas.UI
 
         private void btn_ChooseFolder_Click(object sender, EventArgs e)
         {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Title = "请选择文件";
+            fileDialog.Filter = "txt文件|license.txt";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txt_Path.Text = fileDialog.FileName;
+            }
+            txt_LicenseInfo.Text = readtxt(txt_Path.Text);
 
         }
+        private string readtxt(string path)
+        {
+            var tmp = new StringBuilder();
 
+            System.IO.StreamReader fileReader;
+            using (fileReader = System.IO.File.OpenText(path))
+            {
+                while (!fileReader.EndOfStream)
+                {
+                    tmp.AppendLine(fileReader.ReadLine());
+                }
+            }
+            return tmp.ToString();
+        }
         private void btn_Import_Click(object sender, EventArgs e)
         {
-
+            string InstallDiraddress = this.MyAppSetting.InstallDiraddress;
+            DirectoryInfo TheFolder = new DirectoryInfo(InstallDiraddress);
+            //遍历文件夹
+            foreach (DirectoryInfo NextFolder in TheFolder.GetDirectories())
+            {
+                if (File.Exists(txt_Path.Text))
+                {
+                    string copypath = Path.Combine(NextFolder.FullName, Path.GetFileName(txt_Path.Text));
+                    File.Copy(txt_Path.Text, copypath, true);
+                }
+            }
         }
 
     }
