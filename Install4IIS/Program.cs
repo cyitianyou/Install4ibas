fdfd9a3228902002f2168666bf6b497cd8ba50fa
cyitianyou <cyitianyou@gmail.com>
2016-12-22 17:35:48 +0800

    IIS安装单独调用,发布脚本显示窗口

---
+++ Install4IIS/Program.cs
@@ -0,0 +1,26 @@
+﻿using Install4ibas.Tools.Plugin.IISManager;
+
+namespace Install4IIS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in args)
+            {
+                System.Console.WriteLine(item);
+            }
+            IIISManager manager = IISManagerFactory.New().CreateIISManager();
+            if (args.Length >= 1 && args[0]=="-u") //卸载
+            {
+                if (!manager.IsFullyInstalled())
+                    manager.UninstallIIS();
+            }
+            else
+            {
+                if (!manager.IsFullyInstalled())
+                    manager.InstallIIS();
+            }
+        }
+    }
+}
